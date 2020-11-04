c7f7b69b8c89e855ca8a8ffb7f76c8f9dffcc5eb
Allison Castaneda <allraec@yahoo.com>
2020-11-03 19:39:50 -0600

    forms turned into controlled inputs

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+
+const Form = () => {
+
+    const [user, setUser] = useState(
+        {
+            name: "",
+            email: "",
+            password: "",
+            checkbox: ""
+        }
+    )
+
+    const onChange = (event) => {
+        
+        if (event.target.type === 'checkbox'){
+            setUser({...user, [event.target.name] : event.target.checked});
+        }else{
+            setUser({...user, [event.target.name] : event.target.value});
+        }
+    }
+
+    return(
+        <form>
+            <label htmlFor='name'>
+                Name: 
+                <input type='text' id='name' name='name' value={user.name} onChange={onChange}/>
+            </label>
+            <br />
+            <label htmlFor='email'>
+                Email: 
+                <input type='email' id='email' name='email' value={user.email} onChange={onChange}/>
+            </label>
+            <br />
+            <label htmlFor='password'>
+                Password: 
+                <input type='password' id='password' name='password' value={user.password} onChange={onChange}/>
+            </label>
+            <br />
+            <label htmlFor='checkbox'>
+                Do you agree to the Terms and Condition? 
+                <input type='checkbox' id='checkbox' name='checkbox' onChange={onChange}/>
+            </label>
+            {console.log(user)}
+        </form>
+    )
+}
+
+export default Form;
